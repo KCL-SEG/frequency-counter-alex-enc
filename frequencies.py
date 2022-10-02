60b24f597a5951425bef72ed7c2d4b027cc4d981
Alexandra Encarnacion <alexandra.encarnacion@kcl.ac.uk>
2022-10-02 13:37:42 +0100

    Count the frequency of items in a list

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,16 @@
 
 def frequencies(items):
     frequencies = {}
+    strItems = []
     # Your code goes here
+    for x in items:
+        strItems.append(str(x))
+
+    for i in strItems:
+
+        if (i in frequencies):
+         frequencies[i] += 1
+        else:
+         frequencies[i] = 1
+
     return frequencies
